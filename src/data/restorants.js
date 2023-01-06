3982a5618c76bf9fd2ed8a711bd6e9ea9452190b
huseynmv <huseynmv@yahoo.com>
2023-01-06 09:22:13 +0400

    restaurant data

---
+++ src/data/restorants.js
@@ -0,0 +1,20 @@
+export const restaurantsData = [
+  {
+    id: 1,
+    name: 'Pendir & Chorek',
+    description: `Breakfast, Lunch, Dinner, Brunch, Late Night, Drinks`,
+    mainImage:
+      'https://media-cdn.tripadvisor.com/media/photo-s/1c/a9/81/3d/3.jpg',
+    latitude: 40.36774716871637,
+    longitude: 49.83650235584154,
+  },
+  {
+    id: 2,
+    name: 'Pasifico Lounge & Dining',
+    description: 'Dinner, Late Night, Drinks',
+    mainImage:
+      'https://media-cdn.tripadvisor.com/media/photo-s/10/63/e8/b5/pasifico-terrace-summer.jpg',
+    latitude: 40.36975437650636,
+    longitude: 49.845457709804286,
+  },
+];
