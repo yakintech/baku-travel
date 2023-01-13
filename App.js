debf172f599270cf2054548de105377eabbf93be
cagatayyildiz <yildiz.cagatay@hotmail.com>
2023-01-13 12:30:54 +0300

    splash screen

--- App.js
+++ App.js
@@ -1,11 +1,18 @@
 import {View, Text} from 'react-native';
-import React from 'react';
+import React, { useEffect } from 'react';
 import {NavigationContainer} from '@react-navigation/native';
 import Tab from './src/navigation/index';
 import {FavoritesProvider} from './src/store/context/FavoritesContext';
 import {SafeAreaView} from 'react-native-safe-area-context';
 import {LocationProvider} from './src/store/context/LocationContext';
+import SplashScreen from 'react-native-splash-screen';
 const App = () => {
+
+  useEffect(()=>{
+    SplashScreen.hide();
+  },[])
+
+
   return (
     <>
       <LocationProvider>
